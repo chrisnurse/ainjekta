32b3abd75706928b17655e36e3be116a88d286e4
Chris Nurse <qiksar.apps@gmail.com>
2025-12-17 02:54:58 +0000

    Fix post create process

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,9 @@
 # This command runs when the container is built
-echo "Updating npm..."
-npm install -g npm@latest --silent
-
 echo "Running post-create commands..."
 echo
+
+echo "Install package manager and task runner..."
+npm install -g npm@latest concurrently@latest --silent
 
 echo "Installing backend dependencies..."
 cd /workspaces/ainjekta/backend && npm install --silent && npm update --silent
